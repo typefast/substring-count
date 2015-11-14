1a302e32f4791ad79835b9ff4b170d6c03eec23d
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-14 16:01:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Substrings method takes a word as its first argument, array of valid substrings as second
+And lists each occurence of the substring with its number of occurences.
---
+++ substrings.rb
@@ -0,0 +1,6 @@
+substring_dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+def substrings(word, substring_dictionary)
+  recorded_substrings = {}
+  
+end
+substrings("below", substring_dictionary)
