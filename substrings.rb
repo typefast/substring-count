c934238f40f6c9b9a1818cb955aa1ad8b60659db
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-14 20:00:32 +0000

    iterate through words and hash

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,16 @@
-substring_dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-def substrings(word, substring_dictionary)
-  recorded_substrings = {}
+def substrings(words, substring_dictionary)
+  recorded_words = Hash.new(0) #create a new hash with default value
+  words_to_check = words.downcase.split #downcase and split the words/string
   
+  words_to_check.each do |word| #iterate through each word
+    substring_dictionary.each do |other_word| #iterate through dictionary/hash words
+      if word.include?(other_word) #check if one word is included in another
+        recorded_words[other_word] += 1 #increment 
+      end
+    end
+  end
+  p recorded_words
 end
-substrings("below", substring_dictionary)
+
+substring_dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("Howdy partner, sit down! How's it going?", substring_dictionary)
